a5ad9bf1be5acf282c7b5d6d098176941827ce8c
Daedra <foglzerika@gmail.com>
2020-05-03 22:32:01 -0500

    feat | Creating a mokc function

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
-import {add, subtraction , total} from "./App";
+import { subtraction , total} from "./App";
 
+const add = jest.fn( () =>5);
 
 /* Unit test */
-/* Tests only one function*/
+/* Tests only one thing */
+
 test('add' , ()=>{
     expect(add(2,3)).toBe(5);
+    // Check if the function was wallet n times
+    expect(add).toHaveBeenCalledTimes(1);
+    // Check if the function was called with the proper arguments/
+    expect(add).toHaveBeenCalledWith(1,3);
 });
 
 test ('subtraction' , () =>{
